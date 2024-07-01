b93db891581ff57ad5357aa2fe245bc048358390
marcolanfranchi <marcolanfranchi02@gmail.com>
2024-07-01 13:39:42 -0700

    re-deploying

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -9,7 +9,10 @@ const firebaseConfig = {
     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.REACT_APP_FIREBASE_APP_ID
-  };
+};
+
+console.log('Firebase Config:', firebaseConfig);
+
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
